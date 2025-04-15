2a9518f18ac1fb400f6cadbfed3a3792737add79
harshitha-1911 <chinky.poluka@gmail.com>
2025-04-15 13:50:33 +0530

    f

---
+++ main.js
@@ -0,0 +1,9 @@
+// import greet from "./calc.js"
+// let result=greet(4,5)
+// console.log(result)
+
+import {add} from "./calc.js"
+import greet from "./calc.js"
+let result=greet()
+console.log(result)
+
